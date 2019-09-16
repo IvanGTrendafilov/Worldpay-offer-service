7d867f3ecc12ad86f0d99538193dd0e4cdd682b4
Ivan Trendafilov (V) <itrendafilov@shutterfly.com>
2019-09-17 00:23:12 +0300

    Added swagger configuration and swagger UI

--- offer-gateway/src/main/java/com/trendafilov/ivan/worldpay/offergateway/controllers/OfferGatewayController.java
+++ offer-gateway/src/main/java/com/trendafilov/ivan/worldpay/offergateway/controllers/OfferGatewayController.java
@@ -8,6 +8,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,13 +24,12 @@ public class OfferGatewayController {
     @LoadBalanced
     private RestTemplate restTemplate;
 
-    @RequestMapping("/names")
-    public Collection<String> getReservationNames() {
+    @GetMapping("/descriptions")
+    public Collection<Offer> getOfferDescriptions() {
         final ParameterizedTypeReference<Resources<Offer>> resourcesParameterizedTypeReference = new ParameterizedTypeReference<Resources<Offer>>() {
         };
         final ResponseEntity<Resources<Offer>>
             resposneEntity = this.restTemplate.exchange("http://offer-service:8025/offers", HttpMethod.GET, null, resourcesParameterizedTypeReference);
-        return resposneEntity.getBody().getContent().stream().map(Offer::getDescription).collect(
-            Collectors.toList());
+        return resposneEntity.getBody().getContent();
     }
 }
