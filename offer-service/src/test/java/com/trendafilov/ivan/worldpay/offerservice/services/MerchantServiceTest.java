a6702b45a0f32c7565a23169d356f01637f48eaa
Ivan Trendafilov (V) <itrendafilov@shutterfly.com>
2019-09-18 18:31:18 +0300

    get all merchants

--- offer-service/src/test/java/com/trendafilov/ivan/worldpay/offerservice/services/MerchantServiceTest.java
+++ offer-service/src/test/java/com/trendafilov/ivan/worldpay/offerservice/services/MerchantServiceTest.java
@@ -1,5 +1,6 @@
 package com.trendafilov.ivan.worldpay.offerservice.services;
 
+import com.trendafilov.ivan.worldpay.offerservice.dtos.response.MerchantResponse;
 import com.trendafilov.ivan.worldpay.offerservice.entities.Merchant;
 import com.trendafilov.ivan.worldpay.offerservice.enums.ErrorMessagesEnum;
 import com.trendafilov.ivan.worldpay.offerservice.exceptions.OfferServiceException;
@@ -8,6 +9,7 @@ import com.trendafilov.ivan.worldpay.offerservice.repositories.MerchantRepositor
 import com.trendafilov.ivan.worldpay.offerservice.services.impl.MerchantService;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -15,14 +17,19 @@ import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -38,6 +45,13 @@ public class MerchantServiceTest {
 
     @InjectMocks
     private MerchantService merchantService;
+
+    private MerchantService spy;
+
+    @Before
+    public void setup() {
+        spy = spy(merchantService);
+    }
 
     @Test
     public void test_findMerchantByMerchantIdSuccess() throws OfferServiceException {
@@ -78,5 +92,18 @@ public class MerchantServiceTest {
             assertEquals(e.getMessage(), ErrorMessagesEnum.MERCHANT_NOT_FOUND.getMessage());
             assertTrue(e.getStatusCode() == HttpStatus.BAD_REQUEST.value());
         }
+    }
+
+    @Test
+    public void test_getAllMerchants() {
+        final Merchant mock = mock(Merchant.class);
+        final MerchantResponse merchantResponse = mock(MerchantResponse.class);
+        when(merchantRepository.findAll()).thenReturn(Arrays.asList(mock));
+        doReturn(merchantResponse).when(spy)
+                                  .getMerchantResponseByMerchantEntity(mock);
+        final List<MerchantResponse> allMerchants = spy.getAllMerchants();
+        assertFalse(allMerchants.isEmpty());
+        assertTrue(allMerchants.size() == 1);
+        assertEquals(allMerchants.get(0), merchantResponse);
     }
 }
