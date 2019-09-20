ad9b30eb28a19de7527119b2d6e2ace962bb261d
Ivan Trendafilov (V) <itrendafilov@shutterfly.com>
2019-09-20 18:29:14 +0300

    more integration tests

--- offer-service/src/test/java/com/trendafilov/ivan/worldpay/offerservice/controllers/OfferControllerTest.java
+++ offer-service/src/test/java/com/trendafilov/ivan/worldpay/offerservice/controllers/OfferControllerTest.java
@@ -2,9 +2,15 @@ package com.trendafilov.ivan.worldpay.offerservice.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trendafilov.ivan.worldpay.offerservice.TestConstants;
+import com.trendafilov.ivan.worldpay.offerservice.dtos.requests.OfferRequest;
+import com.trendafilov.ivan.worldpay.offerservice.dtos.requests.ProductItemRequest;
+import com.trendafilov.ivan.worldpay.offerservice.dtos.response.OfferResponse;
+import com.trendafilov.ivan.worldpay.offerservice.enums.ErrorMessagesEnum;
+import com.trendafilov.ivan.worldpay.offerservice.enums.OfferStatus;
 import com.trendafilov.ivan.worldpay.offerservice.exceptions.OfferServiceException;
 import com.trendafilov.ivan.worldpay.offerservice.services.impl.OfferService;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +23,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -55,7 +68,8 @@ public class OfferControllerTest {
     public void test_cancelMerchantOfferWithInvalidMerchant() throws Exception {
         final Long merchantId = new Random().nextLong();
         final Long offerId = new Random().nextLong();
-        doThrow(new OfferServiceException(HttpStatus.BAD_REQUEST.value())).when(offerService)
+        doThrow(new OfferServiceException(ErrorMessagesEnum.MERCHANT_NOT_FOUND.getMessage(),
+                                          HttpStatus.BAD_REQUEST.value())).when(offerService)
                                                                           .cancelMerchantOffer(
                                                                               merchantId.toString(),
                                                                               offerId.toString());
@@ -63,7 +77,76 @@ public class OfferControllerTest {
             put(TestConstants.OFFER_CONTROLLER_URI + "/" + "merchants/" + merchantId
                 + "/offers/" + offerId).headers(getHttpHeaders()))
                .andExpect(status().isBadRequest())
+               .andExpect(
+                   jsonPath("$.message", is(ErrorMessagesEnum.MERCHANT_NOT_FOUND.getMessage())))
                .andDo(print());
+    }
+
+    @Test
+    public void test_insertOffer() throws Exception {
+        final Long merchantId = new Random().nextLong();
+        final List<ProductItemRequest> productItemRequests = Arrays.asList(
+            getProductItemRequest());
+        final OfferRequest offerRequest = getOfferRequest(productItemRequests);
+        when(offerService.insertOfferForMerchant(merchantId.toString(), offerRequest)).thenReturn(
+            getOfferResponse(offerRequest.getCurrency(), offerRequest.getDescription()));
+        mockMvc.perform(
+            post(TestConstants.OFFER_CONTROLLER_URI + "/" + "merchants/" + merchantId).headers(
+                getHttpHeaders())
+                                                                                      .content(
+                                                                                          asJsonString(
+                                                                                              offerRequest)))
+               .andExpect(status().isCreated())
+               .andDo(print());
+    }
+
+    @Test
+    public void test_getActiveOffersForMerchant() throws Exception {
+        final Long merchantId = new Random().nextLong();
+        final List<OfferResponse>
+            offerResponses =
+            Arrays.asList(getOfferResponse(getRandomString(), getRandomString()));
+        when(offerService.getActiveOffersForMerchant(merchantId.toString())).thenReturn(
+            offerResponses);
+        mockMvc.perform(
+            get(TestConstants.OFFER_CONTROLLER_URI + "/" + "merchants/" + merchantId).headers(
+                getHttpHeaders()))
+               .andExpect(status().isOk())
+               .andDo(print())
+               .andExpect(jsonPath("$[0].currency",
+                                   is(offerResponses.get(0)
+                                                    .getCurrency())))
+               .andExpect(jsonPath("$[0].description",
+                                   is(offerResponses.get(0)
+                                                    .getDescription())));
+    }
+
+    private OfferResponse getOfferResponse(final String currency, final String description) {
+        return OfferResponse.builder()
+                            .currency(currency)
+                            .description(description)
+                            .status(OfferStatus.ACTIVE.toString())
+                            .build();
+    }
+
+    private OfferRequest getOfferRequest(final List<ProductItemRequest> productItemRequests) {
+        return OfferRequest.builder()
+                           .description(getRandomString())
+                           .currency(getRandomString())
+                           .price(new BigDecimal(5))
+                           .productItemRequests(productItemRequests)
+                           .build();
+    }
+
+    private ProductItemRequest getProductItemRequest() {
+        return ProductItemRequest.builder()
+                                 .productDescription(getRandomString())
+                                 .productType(getRandomString())
+                                 .build();
+    }
+
+    private String getRandomString() {
+        return RandomStringUtils.random(5);
     }
 
     private HttpHeaders getHttpHeaders() {
